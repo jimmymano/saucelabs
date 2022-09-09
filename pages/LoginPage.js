6a596c9e060c314be303765260b626aee4232dc9
jmano@norwexic.com <jmano@norwexic.com>
2022-09-09 15:46:29 -0500

    Module 3.05: Create a Base Page

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -3,14 +3,13 @@ const USERNAME_INPUT = { id: 'username' }
 const PASSWORD_INPUT = { id: 'password' }
 const SUBMIT_BUTTON = { css: 'button' }
 const SUCCESS_MESSAGE = { css: '.flash.success' }
-const something = {css:'i'}
 const FAILURE_MESSAGE = {css:'.flash.error'}
 const LOGIN_FORM = {id:'login'}
-const BasePage = require('./pages/BasePage')
+const BasePage = require('./BasePage')
 
 class LoginPage extends BasePage {
   constructor(driver) {
-    this.driver = driver
+    super(driver)
   }
 
   async load() {
@@ -21,19 +20,17 @@ class LoginPage extends BasePage {
   }
 
   async authenticate(username, password) {
-    await this.driver.findElement(USERNAME_INPUT).sendKeys(username)
-    await this.driver.findElement(PASSWORD_INPUT).sendKeys(password)
-    await this.driver.findElement(SUBMIT_BUTTON).click()
+    await this.type(USERNAME_INPUT, username)
+    await this.type(PASSWORD_INPUT,password)
+    await this.click(SUBMIT_BUTTON)
   }
 
   async successMessagePresent() {
-    return await this.driver.findElement(SUCCESS_MESSAGE).isDisplayed()
+    return this.isDisplayed(SUCCESS_MESSAGE)
   }
 
   async failureMessagePresent() {
-    console.log(await this.driver.findElement(FAILURE_MESSAGE).isDisplayed())
-    return await 
-    this.driver.findElement(FAILURE_MESSAGE).isDisplayed()
+    return await this.isDisplayed(FAILURE_MESSAGE)
   }
 }
 
