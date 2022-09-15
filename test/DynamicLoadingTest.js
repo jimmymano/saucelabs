d2d1921b7f9200f06128cc5b5a7393ce7d37bec6
jmano@norwexic.com <jmano@norwexic.com>
2022-09-15 00:09:47 -0500

    Module 4.03.1: Scaling tests pt.2 added spec_helper.js functions

--- test/DynamicLoadingTest.js
+++ test/DynamicLoadingTest.js
@@ -1,8 +1,9 @@
 // filename: test/DynamicLoadingTest.js
-const { Builder } = require('selenium-webdriver')
+// const { Builder } = require('selenium-webdriver')
 //const path = require('path')
 const assert = require('assert')
 const DynamicLoadingPage = require('../pages/DynamicLoadingPage')
+require('./spec_helper')
 
 describe('Dynamic Loading', function() {
   this.timeout(30000)
@@ -12,13 +13,13 @@ describe('Dynamic Loading', function() {
     const vendorDirectory =
       // path.delimiter + path.join(__dirname, '..', 'vendor')
       // process.env.PATH += vendorDirectory
-      driver = await new Builder().forBrowser('firefox').build()
-      dynamicLoading = new DynamicLoadingPage(driver)
+      // driver = await new Builder().forBrowser('firefox').build()
+      dynamicLoading = new DynamicLoadingPage(this.driver)
   })
 
-  afterEach(async function() {
-    // await driver.quit()
-  })
+  // afterEach(async function() {
+  //    await driver.quit()
+  // })
 
   it('hidden element', async function() {
     await dynamicLoading.loadExample('1')
--- test/loginTest.js
+++ test/loginTest.js
@@ -1,26 +1,27 @@
 // filename: test/LoginTest.js
-const { Builder } = require('selenium-webdriver')
+// const { Builder } = require('selenium-webdriver')
 const path = require('path')
 const assert = require('assert')
 const LoginPage = require('../pages/LoginPage')
+require('./spec_helper')
 
 //describe is a method from Mocha
 describe('Login', function() {
-    this.timeout(30000)
-    let driver
+    // this.timeout(30000)
+    // let driver
     let login
 
     beforeEach(async function() {
         /* const vendorDirectory =
         path.delimiter + path.join(__dirname, '..', 'vendor')
         process.env.PATH += vendorDirectory */
-        driver = await new Builder().forBrowser('firefox').build()
-        login = new LoginPage(driver)
+        // driver = await new Builder().forBrowser('firefox').build()
+        login = new LoginPage(this.driver)
         await login.load()
     })
-    afterEach(async function() {
-        // await driver.quit()
-    })
+    // afterEach(async function() {
+    //      await driver.quit()
+    // })
     it('Login with valid credentials', async function() {
        
             await login.authenticate('tomsmith', 'SuperSecretPassword!')
